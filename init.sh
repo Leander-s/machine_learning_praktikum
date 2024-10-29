cf28fea78c0915ca53161f1b24fa61cfe47b12c6
Leander <leander.funken@live.de>
2024-10-29 16:17:01 +0100

    Etwas funktioniert..

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ echo "Installing pytorch"
 conda install -y -n ml_gnn_env pytorch==1.3.1 cudatoolkit==9.2 -c pytorch -c nvidia
 
 echo "Installing dgl"
-conda run -n ml_gnn_env python -m pip install dgl==0.4.1
+conda run -n ml_gnn_env python -m pip install dgl-cu92==0.4.1
 
 echo "Installing scikit-learn"
 conda install -y -n ml_gnn_env scikit-learn==0.20.1
