7af6accdc5bdb8a3c05ec82873da26a46ecabe77
Leander <leander.funken@live.de>
2024-10-29 13:21:36 +0100

    version constraints

--- init.sh
+++ init.sh
@@ -15,8 +15,7 @@ echo "Installing scikit-learn"
 conda install -y -n ml_gnn_env scikit-learn==0.20.1
 
 echo "Installing hyperopt"
-conda run -n ml_gnn_env python -m pip -q install bson==0.1.0
-conda run -n ml_gnn_env python -m pip -q install hyperopt==0.2
+conda run -n ml_gnn_env python -m pip -q install hyperopt==0.2 -c ./version-constraints.txt
 
 echo "Installing xgboost"
 conda install -y -n ml_gnn_env xgboost=0.80 -c conda-forge
